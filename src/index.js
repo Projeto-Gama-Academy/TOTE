5c506181337808fc70629720139aaa1266be5653
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-09-25 10:21:56 -0300

    delete some comments

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ const stages = require("./stages");
 bot.create().then((client) => start(client));
 function start(client) {
   client.onMessage((message) => {
-    //console.log(message)
     let resp = stages.step[getStage(message.from)].obj.execute(
       message.from,
       message.body,
@@ -15,6 +14,7 @@ function start(client) {
     );
     for (let index = 0; index < resp.length; index++) {
       const element = resp[index];
+      console.log(element)
       client.sendText(message.from, element);
     }
   });
