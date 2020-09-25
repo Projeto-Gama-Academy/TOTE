4eea5c267a2c4eadbf07d9aa409f7cb3a8f56391
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2020-09-25 12:26:15 -0300

    fix some things

--- src/stages/5.js
+++ src/stages/5.js
@@ -8,7 +8,9 @@ function execute(user, msg) {
   }
 
    return [
-   `*Bahia*\nMuitas moradias e projetos foram concluídos na Bahia no ano de 2019, 
+   `*Suas últimas doações ajudaram aos projetos:*
+   
+*Bahia*\nMuitas moradias e projetos foram concluídos na Bahia no ano de 2019, 
 mas nenhuma construção foi tão marcante quanto a de julho. Em um só fim de semana, foram colocadas
 de pé 21 moradias de emergência, o maior número
 de construções da história da organização no Estado.
